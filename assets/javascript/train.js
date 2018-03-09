3c45cb57eb1c6a658a65c3d80077bfd119778e01
Joel Angell <j.angell07@gmail.com>
2018-03-08 23:32:21 -0800

    Update Firebase project and clear input values

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,12 +1,12 @@
 
 var config = {
-    apiKey: "AIzaSyBQ-v1N-saaaLex2W9sbBzV1dhYp6DP7oQ",
-    authDomain: "train-schedule-e5679.firebaseapp.com",
-    databaseURL: "https://train-schedule-e5679.firebaseio.com",
-    projectId: "train-schedule-e5679",
-    storageBucket: "train-schedule-e5679.appspot.com",
-    messagingSenderId: "986877064035"
-};
+    apiKey: "AIzaSyBHEQ-APLOO3Vgn53SdBvN42qsbttnu7sg",
+    authDomain: "train-scheduler-34c39.firebaseapp.com",
+    databaseURL: "https://train-scheduler-34c39.firebaseio.com",
+    projectId: "train-scheduler-34c39",
+    storageBucket: "train-scheduler-34c39.appspot.com",
+    messagingSenderId: "313340668808"
+  };
 firebase.initializeApp(config);
 
 var database = firebase.database()
@@ -21,25 +21,31 @@ $("#submitBTN").on("click", function (event) {
     var destination = $("#destination").val().trim()
     var trainTime = $("#train-time").val().trim()
     var frequency = $("#frequency").val().trim()
-    
-    
+
+
     trainSchedule.push({
-        name : name,
-        destination : destination, 
+        name: name,
+        destination: destination,
         trainTime: trainTime,
         frequency: frequency
     })
+
+    $("#name").val('')
+    $("#destination").val('')
+    $("#train-time").val('')
+    $("#frequency").val('')
+
 })
 
-trainSchedule.on("child_added", function(snap){
-    
+trainSchedule.on("child_added", function (snap) {
+
     var startTime = moment(snap.val().trainTime, 'HH:mm')
-	var startDisplay = moment(startTime).format('HH:mm')
-	var resetTime = moment(startDisplay, 'HH:mm').subtract(1, 'years')
-	var resetDiff = moment().diff(moment(resetTime), 'minutes')
+    var startDisplay = moment(startTime).format('HH:mm')
+    var resetTime = moment(startDisplay, 'HH:mm').subtract(1, 'years')
+    var resetDiff = moment().diff(moment(resetTime), 'minutes')
     var frequencyDisplay = parseInt(snap.val().frequency)
-	var modulus = resetDiff % frequencyDisplay
-	var minutesAway = frequencyDisplay - modulus
+    var modulus = resetDiff % frequencyDisplay
+    var minutesAway = frequencyDisplay - modulus
     var nextTrain = moment().add(minutesAway, 'minutes')
     var timeDisplay = moment(nextTrain).format("HH:mm")
     $("#table-body").append("<tr><td>" + snap.val().name + "</td><td>" + snap.val().destination + "</td><td>" + frequencyDisplay + "</td><td>" + timeDisplay + "</td><td>" + minutesAway + "</td></tr>")
